0a6dd6751dd2a6330ac3d98e3a24ab514c3e261b
James Totterdell <jatotterdell@gmail.com>
2019-07-24 10:51:25 +0800

    Add randomisation functions

--- R/decision_functions.R
+++ R/decision_functions.R
@@ -8,6 +8,62 @@ prob_best <- function(mat) {
   as.numeric(prop.table(table(factor(max.col(mat), levels = 1:ncol(mat)))))
 }
 
+#' Probability best of linear transformation
+#'
+#' For a supplied linear transform, A, of MC draws, determine
+#' the probability best.
+#'
+#' @param mat Matrix of MC draws
+#' @param A Transformation matrix
+#' @return Numeric vector giving probability best each transformed column is maximum
+#' @export
+prob_best_lin <- function(mat, A) {
+  prob_best(mat %*% t(A))
+}
+
+
+#' Probability for each rank
+#'
+#' Rank each row and return the probability that each arm is ranked at each value
+#'
+#' @param mat Matrix of MC draws
+#' @return Numeric square matrix giving probability each arm is ranked in each place
+#' @export
+prob_rank <- function(mat) {
+  k <- ncol(mat)
+  apply(apply(apply(mat, 1, function(x) rank(-x)), 1, function(z) table(factor(z, levels = 1:k))), 2, prop.table)
+}
+
+
+#' Probability all columns are non-inferior to best column
+#'
+#' @param mat Matrix of MC draws
+#' @param colvec_best A vector of draws from the best
+#' @param eps The reference value for non-inferiority
+#' @return A real number giving the probability that all arms
+#'   are non-inferior to colvec_best.
+#' @export
+prob_all_noninferior <- function(mat, colvec_best, eps) {
+  dmat <- sweep(mat, 1, colvec_best, "-")
+  mean(apply(dmat, 1, function(x) all(x > -eps)))
+}
+
+
+#' Re-distribute allocations
+#'
+#' When some allocations a set to zero for being below some threshold,
+#' re-distribute their mass amongst the remaining arms
+#'
+#' @param w The current allocation weights
+#' @param zero_ind Indices for arms which will receive zero allocation
+#' @return An updated vector of allocation weights
+#' @export
+distribute_alloc <- function(w, zero_ind) {
+  w[zero_ind] <- 0
+  w[-zero_ind] <- w[-zero_ind] / sum(w[-zero_ind])
+  return(w)
+}
+
 
 #' Bayesian response adaptive randomisation
 #'
---
+++ R/randomisation.R
@@ -0,0 +1,149 @@
+#' Mass-weighted urn randomisation
+#'
+#' @param target_alloc The target allocation ratios
+#' @param sample_size The number of allocations to generate
+#' @param alpha Parameter to control imbalance between arms
+#' @return A list detailing the mass-weighted-urn process.
+#' @export
+mass_weighted_urn_design <- function(
+  target_alloc,
+  sample_size,
+  alpha = 4
+) {
+  arms <- length(target_alloc)
+  prob_alloc <- target_alloc / sum(target_alloc)
+  # Masses
+  x <- matrix(0, sample_size + 1, arms)
+  x[1, ] <- alpha * prob_alloc
+  # Sample size
+  n <- matrix(0, sample_size + 1, arms)
+  # Random number
+  y <- runif(sample_size)
+  # Conditional selection probability
+  p <- matrix(0, sample_size + 1, arms)
+  # Imbalance
+  d <- rep(0, sample_size)
+  # Allocation Predictability
+  g <- rep(0, sample_size + 1)
+  # Treatment assignment
+  trt <- rep(0, sample_size)
+
+  imbalance_cap <- sqrt(sum(((alpha - 1)*(1 - prob_alloc) + (arms - 1))^2))
+
+  for(i in 2:(sample_size + 1)) {
+    # Update allocation probabilities
+    p[i - 1, ] <- pmax(alpha * prob_alloc - n[i - 1, ] + (i - 1)*prob_alloc, 0)
+    p[i - 1, ] <- p[i - 1, ] / sum(p[i - 1, ])
+    trt[i-1] <- findInterval(y[i - 1], c(0, cumsum(p[i - 1, ])))
+    # Update sample sizes
+    n[i, ] <- n[i - 1, ]
+    n[i, trt[i-1]] <- n[i, trt[i-1]] + 1
+    # Update urn masses
+    x[i, trt[i-1]] <- x[i - 1, trt[i-1]] - 1 + prob_alloc[trt[i-1]]
+    x[i, -trt[i-1]] <- x[i - 1, -trt[i-1]] + prob_alloc[-trt[i-1]]
+    # Calculate imbalance
+    d[i - 1] <- sqrt(sum((n[i, ] - (i - 1)*prob_alloc)^2))
+    # Calculate allocation predictability
+    g[i] <- d[i - 1] / alpha
+  }
+  return(list(
+    max_imbalance_bound = imbalance_cap,
+    imbalance = d,
+    alloc_predict = g,
+    rand_num = y,
+    trt = trt,
+    mass = x,
+    sample_size = n,
+    selection_prob = p))
+}
+
+#' Bayesian adaptive randomisation method 1
+#'
+#' @param p The probability each arm is best
+#' @param no_alloc_thres The threshold for setting allocations to zero
+#' @param fix_ctrl The fixed allocation amount to control
+#' @return A vector of allocation probabilities for each arm
+#' @export
+brar1 <- function(p, no_alloc_thres = 0.01, fix_ctrl = NULL) {
+  stopifnot(all(p >= 0))
+  p[which(p < no_alloc_thres)] <- 0
+  r <- sqrt(p)
+  w <- r / sum(r)
+
+  if(is.null(fix_ctrl)) {
+    return(w)
+  } else {
+    if(!(fix_ctrl > 0 & fix_ctrl < 1)) stop("fix_ctrl must be between 0 and 1.")
+    # Re-distribute
+    w[1] <- fix_ctrl
+    w[-1] <- w[-1] / sum(w[-1]) * (1 - w[1])
+    return(w)
+  }
+}
+
+#' Bayesian adaptive randomisation method 2
+#'
+#' @param p The probability each arm is best
+#' @param n The current total sample size
+#' @param N The total maximum sample size
+#' @param no_alloc_thres The threshold for setting allocations to zero
+#' @param fix_ctrl The fixed allocation amount to control
+#' @return A vector of allocation probabilities for each arm
+#' @export
+brar2 <- function(p, n, N, no_alloc_thres = 0.01, fix_ctrl = NULL) {
+  stopifnot(all(p >= 0))
+  p[which(p < no_alloc_thres)] <- 0
+  r <- sqrt(p ^ (n / N))
+  w <- r / sum(r)
+
+  if(is.null(fix_ctrl)) {
+    return(w)
+  } else {
+    if(!(fix_ctrl > 0 & fix_ctrl < 1)) stop("fix_ctrl must be between 0 and 1.")
+    # Re-distribute
+    w[1] <- fix_ctrl
+    w[-1] <- w[-1] / sum(w[-1]) * (1 - w[1])
+    return(w)
+  }
+}
+
+#' Bayesian adaptive randomisation method 3
+#'
+#' @param p The probability each arm is best
+#' @param n The current sample size in each arm
+#' @param V The current posterior variance in each arm
+#' @param no_alloc_thres The threshold for setting allocations to zero
+#' @param fix_ctrl The fixed allocation amount to control
+#' @param min_alloc The minimum allocation to an arm
+#' @return A vector of allocation probabilities for each arm
+#' @export
+brar3 <- function(p, n, V, no_alloc_thres = 0.01, fix_ctrl = NULL, min_alloc = 0) {
+  stopifnot(all(p >= 0))
+  stopifnot(all(n > 0))
+  m <- length(p)
+
+  # Set min allocation and dropping
+  w <- rep(min_alloc, m)
+  w[which(p < no_alloc_thres)] <- 0
+  w_rem <- 1 - sum(w)
+  p[which(p < no_alloc_thres)] <- 0
+
+  r <- sqrt(p * V / n)
+  w <- w + w_rem * r / sum(r)
+
+  if(is.null(fix_ctrl)) {
+    return(w)
+  } else {
+    if(!(fix_ctrl > 0 & fix_ctrl < 1)) stop("fix_ctrl must be between 0 and 1.")
+    # Re-distribute
+    w[1] <- fix_ctrl
+    w[-1] <- w[-1] / sum(w[-1]) * (1 - w[1])
+    return(w)
+  }
+}
+
+update_alloc <- function(w, zero_ind) {
+  w[zero_ind] <- 0
+  w[-zero_ind] <- w[-zero_ind] / sum(w[-zero_ind])
+  return(w)
+}
